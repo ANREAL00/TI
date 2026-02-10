c7ad8579fbf171051bb46ecd2c1fbf150fb3c79a
Anton Radzko <anton.redko2626@gmail.com>
2026-02-11 00:12:32 +0300

    change hardcore port

--- Lab1/server/index.js
+++ Lab1/server/index.js
@@ -43,7 +43,7 @@ app.post('/api/encrypt', upload.single('file'), (req, res) => {
     }
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
